d669bc6bfddb943e71bfa281a19dad8d3b35d936
TurtleY0da <timothy.veugelers@gmail.com>
2021-09-22 13:41:29 -0600

    Initial Commit to Draw a Sunrise Assignment

---
+++ script.js
@@ -0,0 +1,13 @@
+// Draw Sunrise Script by Timothy V
+
+// -- Initialize Variables --
+
+// HTML Elements
+let cloudImgEl = document.getElementById("cloudImg");
+
+let cnv = document.getElementById("sunriseCnv");
+
+// Context
+
+/** @type {CanvasRenderingContext2D} */
+let ctx = cnv.getContext("2d");
