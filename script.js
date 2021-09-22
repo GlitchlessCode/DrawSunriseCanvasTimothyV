fe8be9734c8bd7c2f7a513b41cddda9b2ca7e517
TurtleY0da <timothy.veugelers@gmail.com>
2021-09-22 13:51:04 -0600

    Current Final Commit to Draw a Sunrise Assignment

--- script.js
+++ script.js
@@ -11,3 +11,35 @@ let cnv = document.getElementById("sunriseCnv");
 
 /** @type {CanvasRenderingContext2D} */
 let ctx = cnv.getContext("2d");
+
+// -- Canvas --
+cnv.width = 400;
+cnv.height = 400;
+
+// - Draw -
+
+// Style
+ctx.fillStyle = "blue";
+
+// Shape
+ctx.fillRect(0, 0, 400, 400);
+
+// Style
+ctx.fillStyle = "green";
+
+// Shape
+ctx.fillRect(0, 300, 400, 100);
+
+// Style
+ctx.fillStyle = "red";
+
+// Shape
+ctx.beginPath();
+
+ctx.arc(200, 300, 20, Math.PI, 2*Math.PI);
+
+ctx.fill();
+
+// Images
+ctx.drawImage(cloudImgEl, 130, 110);
+ctx.drawImage(cloudImgEl, 170, 90);
